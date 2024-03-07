eb3832be13a194ef52bdf89db5975d3a5bcea683
Pau <paumtz.marp99@gmail.com>
2024-03-07 13:14:12 -0600

    1

---
+++ main.js
@@ -0,0 +1,12 @@
+function miPrimerFuncion(parametro){
+        
+    let variable =
+    document.getElementById(parametro)
+    if(variable.style.display == "none"){
+      variable.style.display = "flex"
+    } else {
+      variable.style.display = "none"
+    }
+  }
+  let nombre="Pau"
+  console.log(nombre);
